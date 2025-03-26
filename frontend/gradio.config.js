4fe57aee2b14efa50844dc1ae9f6c975dbdc6646
jprochazk <pjanv42@gmail.com>
2025-03-26 13:16:54 +0100

    allow linked package

--- frontend/gradio.config.js
+++ frontend/gradio.config.js
@@ -1,5 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+import { searchForWorkspaceRoot } from "vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // `@rerun-io/web-viewer` uses `new URL("./re_viewer_bg.wasm", import.meta.url)` since 0.17,
 // which does not play well with `vite dev`: https://github.com/rerun-io/rerun/issues/6815
@@ -11,6 +17,18 @@ const hack = () => ({
       optimizeDeps: {
         exclude: process.env.NODE_ENV === "production" ? [] : ["@rerun-io/web-viewer"],
       },
+      server: {
+        fs: {
+          allow: [
+            searchForWorkspaceRoot(process.cwd()),
+            // NOTE: hack to allow `new URL("file://...")` in `web-viewer` when it is a linked package
+            fs.realpathSync(
+              path.join(__dirname, "node_modules", "@rerun-io/web-viewer"),
+            ),
+          ],
+        },
+      },
+
     };
   },
 });
